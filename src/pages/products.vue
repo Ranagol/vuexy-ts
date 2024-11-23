1926f7d58a347aa9c62a84070b750ec4d878b8b7
Andor Horvat <andorhorvat@gmail.com>
2024-11-23 21:25:16 +0100

    Create Product type.

--- src/pages/products.vue
+++ src/pages/products.vue
@@ -2,36 +2,29 @@
     <div>
         <h1>Products</h1>
         <ul>
-            <li v-for="product in products" :key="product.id">{{ product.title }}</li>
+            <li 
+                v-for="product in products" 
+                :key="product.id"
+            >{{ product.title }}</li>
         </ul>
     </div>
 </template>
 
 <script setup lang="ts">
 import axiosIns from '@/plugins/axios';
+import type { Product } from '../types/interfaces';
 
-const products = ref([])
+const products = ref<Product[]>([]);
 
 onMounted(async () => {
     try {
-        const response = await axiosIns('products')
+        const response = await axiosIns<Product[]>('products')
         products.value = response.data
     } catch (error) {
         console.error('Error fetching products:', error)
     }
 })
 
-// onMounted(async () => {
-//   console.log('mounted')
-//   const { data, error } = await useFetch('https://fakestoreapi.com/products').json()
-
-//   if (error.value) {
-//     console.error('Error fetching products:', error.value)
-//   } else {
-//     products.value = data.value
-//     console.log(data.value)
-//   }
-// })
 
 
 
---
+++ src/types/interfaces.ts
@@ -0,0 +1,17 @@
+// {
+//     id:1,
+//     title:'...',
+//     price:'...',
+//     category:'...',
+//     description:'...',
+//     image:'...'
+// }
+
+export interface Product {
+    id: number;
+    title: string;
+    price: string;
+    category: string;
+    description: string;
+    image: string;
+}
